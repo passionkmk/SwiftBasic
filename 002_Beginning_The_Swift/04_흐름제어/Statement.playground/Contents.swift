e5b7af2c9bad0e54e4521d6230475e44748579ea
pkmk <passion.kmk@gmail.com>
2019-01-04 10:27:43 +0900

    Add Statement for naming

--- 002_Beginning_The_Swift/04_흐름제어/Statement.playground/Contents.swift
+++ 002_Beginning_The_Swift/04_흐름제어/Statement.playground/Contents.swift
@@ -57,4 +57,34 @@ default:
 // 5. repeat-while
 
 
-var names: [String]
+//var names: [String]
+
+
+//-------------------------------
+// 6. 구문이름
+
+var numbers: [Int] = [3, 2342, 6, 3252]
+
+numbersLoop: for num in numbers {
+    if num > 5 || num < 1 {
+        continue numbersLoop
+    }
+    
+    var count: Int = 0
+    
+    printLoop: while true {
+        print(num)
+        count += 1
+        
+        if count == num {
+            break printLoop
+        }
+        
+        removeLoop: while true {
+            if numbers.first != num {
+                break numbersLoop
+            }
+            numbers.removeFirst()
+        }
+    }
+}
